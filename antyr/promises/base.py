26d82ee5d7bfe9141aee90a79a1ec152b15527f0
Anton Petrov <mrprfrm@gmail.com>
2025-11-15 23:29:31 +0400

    Trio promise approach

--- antyr/promises/base.py
+++ antyr/promises/base.py
@@ -1,125 +1,87 @@
-import asyncio
 import inspect
 from typing import (
     Any,
-    Awaitable,
     Callable,
     Coroutine,
     Generator,
+    Generic,
+    List,
+    Optional,
     ParamSpec,
-    Protocol,
     TypeVar,
     cast,
 )
 
+import trio
 from typing_extensions import Self
 
+# Parameter specification for promise functions
 P = ParamSpec("P")
+# Parameter specification for parent promise functions
+V = ParamSpec("V")
+
+# Promise result type
 R = TypeVar("R")
+# Parent promise result type
 U = TypeVar("U")
-T = TypeVar("T", bound="Promise")
+# Next promise result type
+K = TypeVar("K")
 
 
-class PromiseLike(Protocol[P, R]):
-    def __init__(self, fn: Callable[P, Awaitable[R]]) -> None:
-        """Initialize the promise with a callable and its arguments."""
-        ...
-
-    def __await__(self) -> Generator[None, None, R]:
-        """Resolve the promise and return a result."""
-        ...
-
-    def execute(self, *args: P.args, **kwargs: P.kwargs) -> "PromiseLike[P, R]":
-        """Resolve promise immediately and return a result."""
-        ...
-
-    def then(self, fn: Callable[[R], Awaitable[U]]) -> "PromiseLike[[R], U]":
-        """Chain a callback to be executed when the promise resolves."""
-        ...
-
-
-class Promise(PromiseLike[P, R]):
+class PromiseNode(Generic[P, R]):
     def __init__(
         self,
-        fn: Callable[P, Awaitable[R] | R],
+        fn: Callable[P, Coroutine[None, None, R]],
+        *,
+        parent: Optional["PromiseNode[V, U]"] = None,
     ) -> None:
         self._fn = fn
-        self._tail: asyncio.Future[R] = asyncio.get_running_loop().create_future()
-        self._head: Coroutine[Any, Any, None] | None = None
-        self._processed: bool = False
+        self.__parent: Optional["PromiseNode[Any, Any]"] = parent
+
+    def __copy(self, *, parent: Optional["PromiseNode[V, U]"]) -> "PromiseNode[P, R]":
+        return PromiseNode(self._fn, parent=parent)
 
     def __await__(self) -> Generator[None, None, R]:
-        if self._processed:
-            raise RuntimeError("Promise has already been awaited.")
+        current = self
+        promises: List[PromiseNode[P, R]] = []
+        while current is not None:
+            promises.append(current)
+            current = current.__parent
 
-        if self._head is None:
-            raise RuntimeError(
-                "Promise must be executed before awaiting. Call execute() first."
-            )
+        async def execute() -> R:
+            result = None
+            async with trio.open_nursery():
+                while promises:
+                    handler = promises.pop()
+                    signature = inspect.signature(handler._fn)
+                    if result is None:
+                        bound = signature.bind()
+                    else:
+                        bound = signature.bind(result)
+                    result = await handler._fn(*bound.args, **bound.kwargs)
+            return cast(R, result)
 
-        self._processed = True
-        asyncio.create_task(self._head)
-        return self._tail.__await__()
+        return execute().__await__()
 
-    async def __process(self, *args: P.args, **kwargs: P.kwargs):
-        try:
-            result = self._fn(*args, **kwargs)
-            if inspect.isawaitable(result):
-                result = cast(R, await result)
-            if not self._tail.done():
-                self._tail.set_result(result)
-        except asyncio.CancelledError:
-            if not self._tail.done():
-                self._tail.cancel()
-            raise
-        except Exception as exc:
-            if not self._tail.done():
-                self._tail.set_exception(exc)
+    def then(
+        self, nxt: Callable[[R], Coroutine[None, None, K]] | "PromiseNode[[R], K]"
+    ) -> "PromiseNode[[R], K]":
+        if self.__parent is not None:
+            raise RuntimeError("Cannot chain a root promise that is not initialized.")
 
-    def execute(self, *args: P.args, **kwargs: P.kwargs) -> Self:
-        if self._head is not None:
-            raise RuntimeError("Promise has already been executed.")
-
-        self._head = self.__process(*args, **kwargs)
-        return self
-
-    def then(self, fn: Callable[[R], Awaitable[U] | U]) -> "PromiseLike[[R], U]":
-        nxt = Promise[[R], U](fn)
-        nxt._head = self._head
-
-        def callback(fut: asyncio.Future[R]) -> None:
-            try:
-                if fut.cancelled():
-                    if not nxt._tail.done():
-                        nxt._tail.cancel()
-                elif exc := fut.exception():
-                    if not nxt._tail.done():
-                        nxt._tail.set_exception(exc)
-                else:
-                    asyncio.create_task(nxt.__process(fut.result()))
-            except Exception as exc:
-                if not nxt._tail.done():
-                    nxt._tail.set_exception(exc)
-
-        self._tail.add_done_callback(callback)
+        if isinstance(nxt, PromiseNode):
+            return nxt.__copy(parent=self)
+        nxt = PromiseNode[[R], K](nxt, parent=self)
         return nxt
 
-    def chain(self, promise: T) -> T:
-        promise._head = self._head
 
-        def callback(fut: asyncio.Future[R]) -> None:
-            try:
-                if fut.cancelled():
-                    if not promise._tail.done():
-                        promise._tail.cancel()
-                elif exc := fut.exception():
-                    if not promise._tail.done():
-                        promise._tail.set_exception(exc)
-                else:
-                    asyncio.create_task(promise.__process(fut.result()))
-            except Exception as exc:
-                if not promise._tail.done():
-                    promise._tail.set_exception(exc)
+class Promise(PromiseNode[P, R]):
+    def __init__(self, fn: Callable[P, Coroutine[None, None, R]]) -> None:
+        super().__init__(fn, parent=None)
+        self._head: Optional[Coroutine[None, None, R]] = None
 
-        self._tail.add_done_callback(callback)
-        return promise
+    def init(self, *args: P.args, **kwargs: P.kwargs) -> Self:
+        if self.__parent is not None:
+            raise RuntimeError("Cannot initialize a chained promise directly.")
+        self._head = self._fn(*args, **kwargs)
+        return self
